65c924c2e7c8e698999ac32b2d83728426bd6d9c
genuinelycool <genuinelycool@gmail.com>
2025-05-20 13:57:54 +0545

    07 - 010 Styling the Tags Input component

--- src/components/TagsInput.vue
+++ src/components/TagsInput.vue
@@ -1,20 +1,20 @@
 <template>
-    <div v-for="(tag, index) in tags">
-        {{ tag }}
-        <a @click.prevent="removeTag(index)" href="#">&times;</a>
+    <div class="tags-input-wrapper">
+        <span class="tag-item" v-for="(tag, index) in tags" :key="index">
+            {{ tag }}
+            <a class="remove-tag" @click.prevent="removeTag(index)" href="#">&times;</a>
+        </span>
+
+        <input 
+            class="tag-input"
+            type="text" 
+            v-model.trim="newTag"
+            @keydown.enter="addNewTag"
+            @keydown.delete="removeLastTag"
+            @keydown.tab.prevent="addNewTag"
+            :class="{ 'tag-exits': isTagExits }"
+        />
     </div>
-    <hr />
-
-    {{ newTag }}
-
-    <input 
-        type="text" 
-        v-model.trim="newTag"
-        @keydown.enter="addNewTag"
-        @keydown.delete="removeLastTag"
-        @keydown.tab.prevent="addNewTag"
-        :class="{ 'tag-exits': isTagExits }"
-    />
 </template>
 
 <script>
@@ -56,8 +56,47 @@ export default {
 </script>
 
 <style scoped>
-.tag-exits {
-    color: red;
-    text-decoration: line-through;
+.tag-input.tag-exists {
+  color: red;
+  text-decoration: line-through;
+}
+.tags-input-wrapper {
+  background: #fff;
+  padding: 0.5em;
+  border: 1px solid #dbdbdb;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  min-height: 36px;
+  box-sizing: border-box;
+}
+
+.tag-item {
+  color: #212529;
+  background-color: #eee;
+  margin-right: 0.3em;
+  padding: 0.25em 0.4em;
+  font-size: 75%;
+  line-height: 1;
+  text-align: center;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  padding-right: 1.25em;
+  padding-left: 0.6em;
+}
+
+.tag-input {
+  color: #495057;
+  flex: 1;
+  background: transparent;
+  border: none;
+}
+
+.tag-input:focus {
+  outline: none;
+}
+a.remove-tag {
+  text-decoration: none;
 }
 </style>
